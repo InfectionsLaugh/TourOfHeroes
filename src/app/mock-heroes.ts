6e042ac3af0e7ee398ec01e50e6942e38609979e
Bryan Silva <bryansilva452@gmail.com>
2018-01-28 01:40:50 -0800

    Se agregaron estilos generales y se crearon los datos falsos para hacer pruebas.

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,54 @@
+import { Hero } from './hero';
+
+export const HEROES = [
+  {
+    id: 11,
+    name: "Mr. Nice",
+    identity: "Bob Parr"
+  },
+  {
+    id: 12,
+    name: "Narco",
+    identity: "Joaquín Guzmán"
+  },
+  {
+    id: 13,
+    name: "Bombasto",
+    identity: "Orville Burrell"
+  },
+  {
+    id: 14,
+    name: "Celeritas",
+    identity: "Narda Ramírez"
+  },
+  {
+    id: 15,
+    name: "Magneta",
+    identity: "Erika Lehnsherr"
+  },
+  {
+    id: 16,
+    name: "Rubberman",
+    identity: "John Doe"
+  },
+  {
+    id: 17,
+    name: "Dynama",
+    identity: "Max Eisenhardt"
+  },
+  {
+    id: 18,
+    name: "Dr. IQ",
+    identity: "Jonathan Osterman"
+  },
+  {
+    id: 19,
+    name: "Magma",
+    identity: "Greg Plitt"
+  },
+  {
+    id: 20,
+    name: "Tornado",
+    identity: "Billy Crudup"
+  },
+]
